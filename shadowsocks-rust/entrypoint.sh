a16602f3b4f79f10a233d5729f013f9b617f0878
Coma Grayce <commagray@sunbutt.faith>
2020-10-31 21:35:34 +0200

    Add shadowsocks-rust.

---
+++ shadowsocks-rust/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+SS_PORT_DEFAULT="8388"
+SS_METHOD_DEFAULT="aes-256-gcm"
+SS_PLUGIN_DEFAULT="v2ray-plugin"
+SS_PLUGIN_OPTS_DEFAULT="server;tls;host=$SS_HOST"
+
+/usr/local/bin/ssserver -s "[::]:${SS_PORT:-$SS_PORT_DEFAULT}" -m "${SS_METHOD:-$SS_METHOD_DEFAULT}" -k "$SS_PASSWORD" --plugin "${SS_PLUGIN:-$SS_PLUGIN_DEFAULT}" --plugin-opts "${SS_PLUGIN_OPTS:-$SS_PLUGIN_OPTS_DEFAULT}" "$@"
